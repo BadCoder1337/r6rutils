e2d104531bd60de2a8ec3bdd30920d9a8ae68aff
BadCoder1337 <anton.tiz.musin@gmail.com>
2018-02-28 23:10:31 +0300

    beta_release

--- app.js
+++ app.js
@@ -6,28 +6,30 @@ var r6api = require('r6api')({
   email: cfg.r6sapi_email,
   password: cfg.r6sapi_pass
 });
+
+var cooldown = 24*3600*1000; 
 var rank_game = [
-  'Unranked',
-  'Copper 4',
-  'Copper 3',
-  'Copper 2',
-  'Copper 1',
-  'Bronze 4',
-  'Bronze 3',
-  'Bronze 2',
-  'Bronze 1',
-  'Silver 4',
-  'Silver 3',
-  'Silver 2',
-  'Silver 1',
-  'Gold 4',
-  'Gold 3',
-  'Gold 2',
-  'Gold 1',
-  'Platinum 3',
-  'Platinum 2',
-  'Platinum 1',
-  'DIAMOND'
+  'Без ранга',
+  'Медь 4',
+  'Медь 3',
+  'Медь 2',
+  'Медь 1',
+  'Бронза 4',
+  'Бронза 3',
+  'Бронза 2',
+  'Бронза 1',
+  'Серебро 4',
+  'Серебро 3',
+  'Серебро 2',
+  'Серебро 1',
+  'Золото 4',
+  'Золото 3',
+  'Золото 2',
+  'Золото 1',
+  'Платина 3',
+  'Платина 2',
+  'Платина 1',
+  'АЛМАЗ'
 ];
 
 var redis = require('redis').createClient(process.env.REDIS_URL);
@@ -53,6 +55,76 @@ var router = express.Router();
 var app = express();
 
 //запуск приложения
+
+function ConstSett() {
+  this.id = {};
+  this.id.diamond_role = "";
+  this.id.platinum_role = "";
+  this.id.gold_role = "";             //конструктор говно
+  this.id.silver_role = "";
+  this.id.bronze_role = "";
+  this.id.copper_role = "";
+  this.id.unranked_role = "";
+  this.id.logs_channel = "";
+}
+
+function dlog(id, person, title, content) {
+  var msg = {embed: {
+    color: 3768301,
+    author: {
+      name: person
+    },
+    title: title,
+    description: content,
+    timestamp: new Date(),
+    footer: {
+      icon_url: bot.user.avatarURL
+    }
+  }}
+  try {
+    bot.channels.find('id', id).send(msg);
+  } catch (err) {
+    console.log('[dlog failed]');
+    console.log(msg);
+  }
+}
+
+function checkRank(msg, ubi_id, ids, reg) {
+  r6api.getRanks(ubi_id)
+              .then(result => {
+                if (reg) {
+                  msg.reply('вы успешно зарегистрировались, ваш текущий ранг: '+rank_game[result[0].emea.rank]);
+                } else {
+                  msg.reply('вы успешно обновились, ваш текущий ранг: '+rank_game[result[0].emea.rank]);
+                }              
+              let rank = result[0].emea.rank;
+              let roles = msg.channel.guild.roles;
+              let diamond = roles.find('id', ids.diamond_role);
+              let plat = roles.find('id', ids.platinum_role);
+              let gold = roles.find('id', ids.gold_role);
+              let silver = roles.find('id', ids.silver_role);
+              let bronze = roles.find('id', ids.bronze_role);
+              let copper = roles.find('id', ids.copper_role);
+              let unranked = roles.find('id', ids.unranked_role);
+              let user = msg.member;
+              user.removeRoles([diamond, plat, gold, silver, bronze, copper, unranked], 'Снимаю ранг перед обновлением...');
+              if (diamond!=null & rank == 20) {
+                user.addRole(diamond, '... обновлено!');
+              } else if (plat!=null & rank>=17 & rank<20) {
+                user.addRole(plat, '... обновлено!');
+              } else if (gold!=null & rank>=13 & rank<17) {
+                user.addRole(gold, '... обновлено!');
+              } else if (silver!=null & rank>=9 & rank<13) {
+                user.addRole(silver, '... обновлено!');
+              } else if (bronze!=null & rank>=5 & rank<9) {
+                user.addRole(bronze, '... обновлено!');
+              } else if (copper!=null & rank>=1 & rank<5) {
+                user.addRole(copper, '... обновлено!');
+              } else if (unranked!=null & rank == 0) {
+                user.addRole(unranked, '... обновлено!');
+              } 
+              });
+}
 
 console.log('[Start]');
 
@@ -92,7 +164,7 @@ app.use(function(err, req, res, next) {
 });
 
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'R6Rutils' });
+  res.render('index', { title: 'R6Rutils', comeback: ''});
 });
 
 router.get('/admin/:pass', function(req, res, next) {
@@ -100,38 +172,42 @@ router.get('/admin/:pass', function(req, res, next) {
   redis.get('pass_'+req.params.pass, function(err, reply) {
     var guild_id = reply;
     if (guild_id) {
+      redis.get('guild_'+guild_id, function(err, reply) {
+        var settings = JSON.parse(reply);
+        dlog(settings.id.logs_channel,'IP '+req.get('X-Forwarded-For'), 'Доступ к настройкам бота через веб', 'https://r6rutils.herokuapp/admin/'+req.params.pass);
+
         let guild = bot.guilds.find('id', guild_id);
         var roles_list = guild.roles.array();
-        //console.log('[AAAAAAAAAAAAAAAAAAAAAAAAA]'+roles_list.length);
-        var select = '<option value="-1">0. Не выбрано</option>';
-        //console.log(roles_list[3].name);
-        for (var i=0; i < roles_list.length; i++) {
-          //console.log(roles_list[i].name);
-          select+='<option value="'+i+'">'+(i+1)+'. '+roles_list[i].name+'</option>';
-        }
-        res.render('admin', { title: 'Привязка ролей на '+guild.name, roles: select});
+        var channels_list = guild.channels.filterArray(chl => chl.type = 'text');
+        //for (var i=0; i < roles_list.length; i++) {
+        //  select+='<option value="'+i+'">'+(i+1)+'. '+i].name+'</option>';
+        //}
+        res.render('admin', { title: 'Настройка '+guild.name, roles: roles_list, channels: channels_list, settings: settings});
+    
+
+      })
     } else {
       res.redirect('/');
     }
   })
 })
 
-router.get('/rank/:nick', function(req, res, next) {
-  //console.log(req.body.pass);
-  if (!req.params.nick=='') {
-    r6api.findByName(req.params.nick)
-      .then(result => {
-        //console.log(result[0].id);
-        r6api.getRanks(result[0].id)
-          .then(result => {
-            console.log(result[0].emea.rank);
-            res.render('index', { title: 'Your rank is: '+rank_game[result[0].emea.rank]});
-          })
-      })
-  } else {
-    res.redirect('/');
-  }
-});
+// router.get('/rank/:nick', function(req, res, next) {
+//   //console.log(req.body.pass);
+//   if (!req.params.nick=='') {
+//     r6api.findByName(req.params.nick)
+//       .then(result => {
+//         //console.log(result[0].id);
+//         r6api.getRanks(result[0].id)
+//           .then(result => {
+//             console.log(result[0].emea.rank);
+//             res.render('index', { title: 'Your rank is: '+rank_game[result[0].emea.rank]});
+//           })
+//       })
+//   } else {
+//     res.redirect('/');
+//   }
+// });
 
 router.post('/admin/:pass', function(req, res) {
   console.log('[Web] Posting to admin panel: '+req.params.pass);
@@ -140,31 +216,23 @@ router.post('/admin/:pass', function(req, res) {
     if (guild_id) {
         let guild = bot.guilds.find('id', guild_id);
         var roles_list = guild.roles.array();
-        let settings = {
-          "id": {
-            "diamond_role": "",
-            "platinum_role": "",
-            "gold_role": "",
-            "silver_role": "",
-            "bronze_role": "",
-            "copper_role": "",
-            "unranked_role": ""
-          }
-        };
+        let settings = new ConstSett();
 
-        console.log(req.body);
+        //console.log(req.body);
         
-        if (req.body.diamond>=0) {settings.id.diamond_role = roles_list[req.body.diamond].id;}
-        if (req.body.platinum>=0) {settings.id.platinum_role = roles_list[req.body.platinum].id;}
-        if (req.body.gold>=0) {settings.id.gold_role = roles_list[req.body.gold].id;}
-        if (req.body.silver>=0) {settings.id.silver_role = roles_list[req.body.silver].id;}
-        if (req.body.bronze>=0) {settings.id.bronze_role = roles_list[req.body.bronze].id;}
-        if (req.body.copper>=0) {settings.id.copper_role = roles_list[req.body.copper].id;}
-        if (req.body.unranked>=0) {settings.id.unranked_role = roles_list[req.body.unranked].id;}
+        settings.id.diamond_role = req.body.diamond;
+        settings.id.platinum_role = req.body.platinum;
+        settings.id.gold_role = req.body.gold;
+        settings.id.silver_role = req.body.silver;
+        settings.id.bronze_role = req.body.bronze;
+        settings.id.copper_role = req.body.copper;
+        settings.id.unranked_role = req.body.unranked;
+        settings.id.logs_channel = req.body.logs;
 
         redis.set('guild_'+guild_id, JSON.stringify(settings));
 
-        res.redirect('/admin/'+req.params.pass);
+        res.render('index', { title: 'Изменения сохранены', comeback: '/admin/'+req.params.pass })
+        //res.redirect('/admin/'+req.params.pass);
     } else {
       res.redirect('/');
     }
@@ -181,41 +249,32 @@ bot.on('ready', () => {
 
 bot.on('guildCreate', guild => {
   let pass = Math.random().toString(36).substring(2, 15);
-  let settings = {
-    "id": {
-      "diamond_role": "",
-      "platinum_role": "",
-      "gold_role": "",
-      "silver_role": "",
-      "bronze_role": "",
-      "copper_role": "",
-      "unranked_role": ""
-    }
-  };
   redis.set("pass_"+pass, guild.id);
-  redis.set("guild_"+guild.id, JSON.stringify(settings))
-  guild.owner.send("https://r6rutils.herokuapp.com/admin/"+pass+" - это страница настроек сервера");
+  redis.set("guild_"+guild.id, JSON.stringify(new ConstSett()))
+  guild.owner.send("https://r6rutils.herokuapp.com/admin/"+pass+" - это страница настроек сервера, держите ее в секрете");
 });
 
 bot.on('message', message => {
   //console.log(message.content);
-  if (!message.author.bot & message.guild!=undefined & message.content.startsWith('$register')) {
-    console.log('[Registration] Start. Nick: '+message.content.slice(10));
+  if (!message.author.bot & message.guild!=undefined & message.content.startsWith('$rank')) {
+    console.log('[Registration] Start. Nick: '+message.content.slice(6));
     redis.get('guild_'+message.guild.id, function(err, reply) {
       redis.get('guild_'+message.guild.id, function(err, reply) {
         let settings = JSON.parse(reply);
         var ids = settings.id;
         var prefix = settings.prefix;
-        var nick = message.content.slice(10);
+        var nick = message.content.slice(6);
         redis.get('user_'+message.author.id, function(err, reply) {
           try {
-            var last_update = JSON.parse(reply).last_update;
-            var can_update = new Date().getTime() - last_update > 3*24*3600*1000;
+            let user = JSON.parse(reply);
+            var last_update = user.last_update;
+            var ubisoft_id = user.ubisoft_id;
+            var can_update = new Date().getTime() - last_update > cooldown;
           } catch (err) {
             console.log('[Registration] New User');
-            can_update = true;
+            var new_user = true;
           }
-          if (nick && can_update) {
+          if (new_user) {
             console.log('[Registration] Searching at r6db.com');
             r6api.findByName(nick)
             .then(result => {
@@ -224,43 +283,28 @@ bot.on('message', message => {
                 "last_update": new Date().getTime()
               } 
               redis.set('user_'+message.author.id,JSON.stringify(user));
-              r6api.getRanks(result[0].id)
-              .then(result => {
-              //console.log(result[0].emea.rank)
-              message.channel.send(rank_game[result[0].emea.rank]);
-              let rank = result[0].emea.rank;
-              let roles = message.channel.guild.roles;
-              let diamond = roles.find('id', ids.diamond_role);
-              let plat = roles.find('id', ids.platinum_role);
-              let gold = roles.find('id', ids.gold_role);
-              let silver = roles.find('id', ids.silver_role);
-              let bronze = roles.find('id', ids.bronze_role);
-              let copper = roles.find('id', ids.copper_role);
-              let unranked = roles.find('id', ids.unranked_role);
-              let user = message.member;
-              if (diamond!=null & rank == 20) {
-                user.addRole(diamond);
-              } else if (plat!=null & rank>=17 & rank<20) {
-                user.addRole(plat);
-              } else if (gold!=null & rank>=13 & rank<17) {
-                user.addRole(gold);
-              } else if (silver!=null & rank>=9 & rank<13) {
-                user.addRole(silver);
-              } else if (bronze!=null & rank>=5 & rank<9) {
-                user.addRole(bronze);
-              } else if (copper!=null & rank>=1 & rank<5) {
-                user.addRole(copper);
-              } else if (unranked!=null & rank == 0) {
-                user.addRole(unranked);
-              } 
-              });
+
+              checkRank(message, result[0].id, ids, true);
+
+              dlog(ids.logs_channel, message.author.username+'#'+message.author.discriminator, 'Пользователь зарегистрирован', 'Ник в игре: '+nick);
+
+            }, reject => {
+              message.reply('пользователь с никнеймом '+nick+' не найден!');
             });
+          } else if (nick && can_update) {
+            console.log('[Registration] Updating '+nick);
+
+            checkRank(message, ubisoft_id, ids, false);
+
+            dlog(ids.logs_channel, message.author.username+'#'+message.author.discriminator, 'Пользователь обновлен', 'Ник в игре: '+nick);
+
           } else if (nick && !can_update) {
-            let timeDiff = Math.round(Math.abs(3*24*3600*1000 - new Date().getTime() + JSON.parse(reply).last_update)/1000);
-            let diffDays = Math.ceil(timeDiff / 86400)-1;
+            let timeDiff = Math.round(Math.abs(cooldown - new Date().getTime() + JSON.parse(reply).last_update)/1000);
+            //let diffDays = Math.ceil(timeDiff / 86400)-1;
             let diffHours = Math.ceil((timeDiff % 86400) / 3600);
             let diffMinutes = Math.ceil((timeDiff % 3600) / 60);
-            message.reply('следующее обновление вашего ранга возможно через: **'+diffDays+' д. '+diffHours+' ч. '+diffMinutes+' м.**');
+            message.reply('следующее обновление вашего ранга возможно через: **'+diffHours+' ч. '+diffMinutes+' м.**');
+            console.log('[Registration] Cooldown dont expired!');
           }
         })
       });
--- update.bat
+++ update.bat
@@ -2,4 +2,3 @@ git add .
 git commit -m 'fix'
 git push heroku master
 heroku ps:scale web=1 --app r6rutils
-heroku logs --tail --app r6rutils
